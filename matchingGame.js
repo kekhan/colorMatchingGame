c006f5f0a44ea66ee46992983b66f141dc8bfc31
Kerin Khan <kerink585@gmail.com>
2018-01-23 00:12:59 -0800

    added js code to fix later

--- matchingGame.js
+++ matchingGame.js
@@ -1,7 +1,70 @@
+var arrClick = [];
+var matchedArr =[];
+var userTurn = false;
+var tick = 0;
+
+function startGame(){
+	userTurn = true;
+	timer(tick);
+
+}
+
+
 function getClick(btn){
+	if(userTurn){
+
+		var element = btn.value;
+
+		if(element == "1"){
+			btn.style.backgroundColor = "blue";
+		}
+		else if (element == "2"){
+			btn.style.backgroundColor ="pink";
+		}
+		else if (element == "3"){
+			btn.style.backgroundColor = "yellow";
+		}
+		else if (element == "4"){
+			btn.style.backgroundColor = "purple";
+		}
+
+		arrClick.push(element,btn);
+
+		if(arrClick.length == 2){
+			check2loop(arrClick);
+			arrClick =[];
+		}
+
+	}
 	
-	var match = [1,1,2,2,3,3,4,4,5];
-	var element = btn.value;
-	element = 2;
-	console.log(element);
+
+}
+
+function check2loop(arr,btn){
+	var first = arr[0];
+	var second = arr[1];
+	 if (first == second){
+
+	 	matchedArr.push(first);
+	 	matchedArr.push(second);
+	 	element = document.getElementByClassName('button' + first)[0];
+	 	element.style.backgroundColor = "orange";
+	 }
+	
+}
+
+function timer(tick){
+	var timer = setTimeOut(function(){
+		tick = tick + 1;
+
+	},1000);
+	clearTimeOut(timer);
+}
+
+function checkWin(){
+	if(matchedArr.length >= 8){
+		document.getElementsByTagName('h1').innerhtml = "Great Job!";
+		console.log(tick);
+
+	}
 }
